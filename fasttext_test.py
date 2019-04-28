e707146009768335b048237f0f6f0666f5f8b57f
Joanna Hoang <hoangj21@up.edu>
2019-04-28 13:30:48 -0700

    Got fasfText working!!!

---
+++ fasttext_test.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 27 21:40:51 2019
+
+@author: joann
+"""
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+import ftfy
+import codecs
+import random
+import fastText
+import os
+from youtubepred_preprocessing import process
+
+
+def print_results(N, p, r):
+    print("N\t" + str(N))
+    print("P@{}\t{:.3f}".format(1, p))
+    print("R@{}\t{:.3f}".format(1, r))
+    
+#df = pd.read_csv('C:/Users/joann/Documents/CS491 Machine Learning/shark/YouTubeConcatWhaleShark_20190317.arff',skiprows=6)
+df = process()
+#print(df)
+data = df.values
+for each in data:
+    each[0] = ftfy.fix_encoding(each[0])
+
+#print(data)
+train_output = codecs.open("fasttext_train.txt","w",encoding='utf-8')
+test_output = codecs.open("fasttext_test.txt","w",encoding='utf-8')
+ft_line = ""
+percent_test_data =.2
+for each in data:
+    if type(each[1])==str:
+        fasttext_line = "__label__" + each[1] + " " +  str(each[0])
+        if random.random() <= percent_test_data:
+            test_output.write(fasttext_line + "\n")
+        else:
+            train_output.write(fasttext_line + "\n")
+       
+test_output.close()
+train_output.close()  
+
+classifier = fastText.train_supervised('fasttext_train.txt', epoch=20,wordNgrams=3)
+result = classifier.test('fasttext_test.txt')
+print_results(*result)
+
--- youtubepred_preprocessing.py
+++ youtubepred_preprocessing.py
@@ -6,19 +6,14 @@ Created on Sat Apr 27 14:32:07 2019
 """
 
 import pandas as pd
-
-df = pd.read_csv('C:/Users/joann/Documents/CS491 Machine Learning/shark/YouTubePredictor_20190319.arff',skiprows=8,sep="]',",engine='python')
-file = open("youtubepred.txt","w") 
-
-counter = 0
-for sentence in df['text']:
-    df.at[counter,'text'] = df.at[counter,'text'].replace('[',"")
-    df.at[counter,'text'] = df.at[counter,'text'].replace(']',"")
-    df.at[counter,'text'] = df.at[counter,'text'].replace('\\',"")
-    df.at[counter,'text'] = df.at[counter,'text'].replace('\'',"")
-    df.at[counter,'text'] = df.at[counter,'text'].replace('\"',"")
-    counter+=1
-    file.write("%s\n" % sentence) 
-   
-print(df.columns)
-print(df)        
+def process():
+    df = pd.read_csv('C:/Users/joann/Documents/CS491 Machine Learning/shark/YouTubePredictor_20190319.arff',skiprows=8,sep="]',",engine='python')
+    counter = 0
+    for sentence in df['text']:
+        df.at[counter,'text'] = df.at[counter,'text'].replace('[',"")
+        df.at[counter,'text'] = df.at[counter,'text'].replace(']',"")
+        df.at[counter,'text'] = df.at[counter,'text'].replace('\\',"")
+        df.at[counter,'text'] = df.at[counter,'text'].replace('\'',"")
+        df.at[counter,'text'] = df.at[counter,'text'].replace('\"',"")
+        counter+=1
+    return df   
